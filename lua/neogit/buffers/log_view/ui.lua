aff2a66e703fb3138fb8f1c8814f629d9f6efc1f
luathn <luat1203@gmail.com>
2021-07-21 17:56:30 +0700

    tmp

--- lua/neogit/buffers/log_view/ui.lua
+++ lua/neogit/buffers/log_view/ui.lua
@@ -20,12 +20,47 @@ local M = {}
 -- |
 
 M.Commit = Component.new(function(commit)
+  months = {
+    Jan = 1,
+    Feb = 2,
+    Mar = 3,
+    Apr = 4,
+    May = 5,
+    Jun = 6,
+    Jul = 7,
+    Aug = 8,
+    Sep = 9,
+    Otc = 10,
+    Nov = 11,
+    Dec = 12,
+  }
+
+  str="Sun Jul 18 18:29:40 2021 +0200"
+  pattern="%a+ (%a+) (%d+) (%d+):(%d+):(%d+) (%d+) (+%d+)"
+
+  month, day, hour, min, sec, year, tz=str:match(pattern) 
+
+  -- print(os.time({tz=tz,day=day,month=months[month],year=year,hour=hour,min=min,sec=sec}))
+
+  -- s="Sat, 29 Oct 1994 19:43:31 GMT"
+  -- p="(%a+), (%d+) (%a+) (%d+) (%d+):(%d+):(%d+) GMT"
+  -- week_day,day,month,year,hour,min,sec=s:match(p)
+
+  space_len_1 = vim.api.nvim_eval('winwidth(0)') - string.len(commit.description[1]) -
+    string.len(("* "):rep(commit.level + 2)) - 70
+  space_len_2 = 20 - string.len(commit.author_name)
+    -- print(asterisk_len)
   return col {
     row { 
-      text(("* "):rep(commit.level + 1), { highlight = "Character" }), 
-      text(commit.oid:sub(1, 7), { highlight = "Number" }), 
-      text " ", 
-      text(commit.description[1]) 
+      text(("* "):rep(commit.level + 1), { highlight = "Character" }),
+      text(commit.oid:sub(1, 7), { highlight = "Number" }),
+      text " ",
+      text(commit.description[1]),
+      text((" "):rep(space_len_1)),
+      text(commit.author_name, { highlight = "Number" }),
+      text((" "):rep(space_len_2)),
+      text "---",
+      -- text(months["Jan"]),
     },
     col.hidden(true).padding_left((commit.level + 1) * 2) {
       row {
